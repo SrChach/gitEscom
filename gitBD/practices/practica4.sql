8d823461ced3323bd59a0298a971d73a6eb03d72
Ignacio Martinez <gsikmo@gmail.com>
2017-09-20 01:39:54 -0500

    p4: Like & regexp mySQL

--- gitBD/practices/practica4.sql
+++ gitBD/practices/practica4.sql
@@ -63,10 +63,6 @@ create table prodVendidos(
 	precioVenta float
 );
 
-/*
-	buscar kendo ui
-*/
-
 insert into cliente (idCliente, nombre, apPat, apMat, rfc, sexo, telefono, credito, deuda, fechaNac, correo, fechaAlta, pais, estado, delegacion, colonia, calle, numExt, numInt, CodPostal)	values 
 	(1, 'Ana', 'Martinez', 'Villarreal', 'rfc', 'F', '1728394650', 1200.00, 500.00, '2005/05/09', '1@hotmail.com', '2000/01/01', 'Mexico', 'DF', 'Coyoacan', 'roma', 'calle', 13, 1, '42700'),
 	(2, 'Annette', 'Juarez', 'Gomez', 'rfc', 'F', '1928406342', 900.00, 150.00, '2003/10/10', '2@hotmail.com', '2000/01/01', 'Mexico', 'DF', 'iztacalco', 'jupiter', 'calle', 04, 2, '42700'),
@@ -78,62 +74,7 @@ insert into cliente (idCliente, nombre, apPat, apMat, rfc, sexo, telefono, credi
 	(8, 'Elizabeth', 'Rodriguez', 'de la Torre', 'rfc', 'F', '4466772288', 4000.00, 67.00, '1991/01/01', '8@hotmail.com', '2000/01/01', 'Mexico', 'DF', 'atzcapozalco', 'jupiter', 'calle', 11, 1, '42700'),
 	(9, 'Edgar', 'Catalan', 'Salgado', 'rfc', 'M', '7733664499', 1200.00, 1200.00, '1993/12/12', '9@hotmail.com', '2000/01/01', 'Mexico', 'DF', 'iztacalco', 'roma', 'calle', 33, 2, '42700'),
 	(10, 'Flor', 'Alamilla', 'de la O', 'rfc', 'F', '1122334455', 900.00, 10.00, '1997/07/07', '10@hotmail.com', '2000/01/01','Mexico', 'DF', 'atzcapozalco', 'pluton', 'calle', 66, 2, '42700');
-
-/*Consulta1*/
-select concat (nombre, ' ', apPat, ' ', apMat) from cliente;
-
-/*Consulta2*/
-select concat (nombre, ' ', apPat) from cliente;
-
-/*Consulta3*/
-select concat (apPat, ' ', apMat, ' ', nombre) from cliente;
-
-/*Consulta4*/
-select concat ('El cliente ', nombre, ' se apellida ', apPat) from cliente where nombre='Edgar';
-
-/*Consulta5*/
-select concat (nombre, ' vive en ', estado, ' ', delegacion, ' ', calle, ' ', numExt) as "Domicilio" from cliente where nombre='Edgar';
-
-/*Consulta6*/
-select (credito - deuda) as "Credito disponible" from cliente;
-
-/*Consulta7*/
-select concat (nombre, ' el 15% de tu deuda es: ', (deuda* 0.15)) from cliente;
-
-/*Consulta8*/
-select nombre, deuda, (deuda*0.1) as "Comisión(10%)", (deuda+(deuda*0.1)) as "Deuda + Comisión" from cliente;
-
-/*Consulta9*/
-select nombre as "pagos mensuales a", round((deuda/6), 2) as "6 meses", round((deuda/12), 2) as "12 meses", 
-	round((deuda/24), 2) as "24 meses" from cliente;
-
-/*Consulta10*/
-select nombre, apPat, apMat, deuda, round((deuda/6)*0.1, 2) as "a 6 meses con cargo del 10%" from cliente;
-
-/*Consulta11*/
-select nombre, deuda, round((deuda*0.7),2) as "deuda-30%" from cliente;
-
-/*Consulta12*/
-select nombre as "pagos mensuales(anticipado ya 30%) a", round(((deuda*0.7)/6), 2) as "6 meses", 
-	round(((deuda*0.7)/12), 2) as "12 meses", round(((deuda*0.7)/24), 2) as "24 meses" from cliente;
-
-/*Consulta13*/
-select concat ('el crédito máximo de ', nombre, ' es ', credito) from cliente where nombre='Edgar';
-
-/*Consulta14*/
-select concat (nombre, ' tiene un crédito de ', credito, ' y debe ', deuda, ' le queda disponible ', 
-		(credito-deuda)) from cliente where nombre='Edgar';
-
-/*Consulta15*/
-select concat (nombre, ' con crédito a 6 meses debe pagar ', deuda, '. Mensualmente pagará ', 
-		(deuda/6)) from cliente where nombre='Edgar';
-
-/*Consulta16*/
-select concat ('Estimado cliente ', nombre, ' ', apPat, ' le recordamos que tiene una deuda de ', deuda, 
-		' pesos, le ofrecemos la posibilidad de pagar el 40%[', round((deuda*0.4), 2), 
-		' pesos] y el resto a 6 meses con el 10% de interés, quedando en ',  round(((deuda*0.6*1.1)/6), 2), 
-		' pesos mensuales') as "aviso" from cliente where nombre='Edgar';
-
+	
 /*
 	Inicia practica 4 
 */
