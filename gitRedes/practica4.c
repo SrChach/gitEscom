6324f12faa7ebd8d746cdbf015614af80db7480d
Ignacio Martinez <gsikmo@gmail.com>
2017-10-30 14:50:06 -0600

    practica de redes añadida

--- gitRedes/practica4.c
+++ gitRedes/practica4.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main(){
 	
 	int uno, dos, tres, cuatro;
@@ -12,7 +10,6 @@ int main(){
 	printf("\nClase de IP:  ");
 	if( 1<=uno && uno<=127 ){
 		printf("A\nDireccion de Red: %d.0.0.0\nMascara de Red: 255.0.0.0\nDireccion de Broadcast: %d.255.255.255\nRango de direcciones de Host: %d.0.0.1 -> %d.255.255.254", uno, uno, uno, uno);
-		
 	} else if(128<=uno && uno<=191){
 		printf("B\nDireccion de Red: %d.%d.0.0\nMascara de Red: 255.255.0.0\nDireccion de Broadcast: %d.%d.255.255\nRango de direcciones de Host: %d.%d.0.1 -> %d.%d.255.254", uno, dos, uno, dos, uno, dos, uno, dos);
 	} else if(192<=uno && uno <=223){
