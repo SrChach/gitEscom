a65d09b24a2610176f9d16cf7457a1fb433b9731
Ignacio Martinez <gsikmo@gmail.com>
2018-06-09 21:09:10 -0500

    añadidos programas java/Hadoop

---
+++ 6_Semestre/mineria/bigCounter.java
@@ -0,0 +1,187 @@
+import java.io.*;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class BigCounter {
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private Text llave = new Text();
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            if( line.length() == 0 ) return;
+
+            StringTokenizer tokenizer = new StringTokenizer(line, ",");
+System.out.println("#### "+line);
+
+            String sx = tokenizer.nextToken();
+            String sy = tokenizer.nextToken();
+            int x = Integer.parseInt(sx);
+            int y = Integer.parseInt(sy);
+            llave.set("x");
+            context.write(llave, new IntWritable(x));
+
+            llave.set("x2");
+            context.write(llave, new IntWritable(x * x));
+
+            llave.set("y");
+            context.write(llave, new IntWritable(y));
+
+            llave.set("xy");
+            context.write(llave, new IntWritable(x * y));
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        int n = 0;
+        int sumX = 0;
+        int sumX2 = 0;
+        int sumY = 0;
+        int sumXY = 0;
+
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            String llave = key.toString();
+            if (llave.compareTo("x") == 0) {
+                for (IntWritable val : values) {
+                    sumX += val.get();
+                    n += 1;
+                }
+            }
+
+            if (llave.compareTo("y") == 0) {
+                for (IntWritable val : values) {
+                    sumY += val.get();
+                }
+            }
+
+            if (llave.compareTo("x2") == 0) {
+                for (IntWritable val : values) {
+                    sumX2 += val.get();
+                }
+            }
+
+            if (llave.compareTo("xy") == 0) {
+                for (IntWritable val : values) {
+                    sumXY += val.get();
+                }
+            }
+           // context.write(new Text("r"), new IntWritable(1));
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+
+            double b = 0;
+            double m = 0;
+
+            m = (double) (n * sumXY - sumX * sumY) / (double) (n * sumX2 - (sumX * sumX));
+            b = ((double)sumY - m * sumX) / (double) n;
+
+            BufferedWriter salida = new BufferedWriter(new FileWriter("/home/cic/regr.dat",false));
+            salida.write(""+n+"\n"+b+"\n"+m+"\n\n");
+            salida.close();
+
+            System.out.println("n="+n+"\nb=" + b + "  \nm=" + m);
+            //context.write(new Text("b"), new IntWritable(2));
+        }
+    }
+
+
+    public static class Map2 extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+        private Text llave = new Text();
+        double m = 0;
+        double b = 0;
+        double suma = 0;
+        double n = 0;
+
+        protected void setup(Context context) throws IOException, InterruptedException {
+            BufferedReader entrada = new BufferedReader(new FileReader("/home/cic/regr.dat"));
+
+            String dato = entrada.readLine();
+            n = Double.parseDouble(dato);
+            dato = entrada.readLine();
+            b = Double.parseDouble(dato);
+            dato = entrada.readLine();
+            m = Double.parseDouble(dato);
+            entrada.close();
+        }
+
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            if (line.length() == 0) return;
+
+            StringTokenizer tokenizer = new StringTokenizer(line, ",");
+            System.out.println("==== " + line);
+
+            String sx = tokenizer.nextToken();
+            String sy = tokenizer.nextToken();
+            int x = Integer.parseInt(sx);
+            int y = Integer.parseInt(sy);
+
+            double yGorro = m * x + b;
+
+            double errorY = Math.pow(y - yGorro,2);
+            suma += errorY;
+
+            System.out.println("X="+x+"  Y="+y+"  y="+yGorro);
+
+            llave.set("e");
+            context.write(llave, new DoubleWritable(errorY));
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+
+         System.out.println("Error=" + suma+"   "+ Math.sqrt(suma / n));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        Job job = new Job(conf, "Regresion");
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path("/home/cic/datosr2.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("/home/cic/zz1"));
+
+        job.waitForCompletion(true);
+
+
+        Configuration conf2 = new Configuration();
+
+        Job job2 = new Job(conf2, "Regresion error");
+
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(DoubleWritable.class);
+
+        job2.setMapperClass(Map2.class);
+
+        job2.setInputFormatClass(TextInputFormat.class);
+
+        FileInputFormat.addInputPath(job2, new Path("/home/cic/datosr2.txt"));
+        FileOutputFormat.setOutputPath(job2, new Path("/home/cic/zz2"));
+
+        job2.waitForCompletion(true);
+    }
+}
---
+++ 6_Semestre/mineria/kmeans.java
@@ -0,0 +1,161 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.lang.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class kmeans {
+
+    public static class KmeansMapper extends Mapper<LongWritable, TwoDPointWritable, IntWritable, TwoDPointWritable>{
+        public final static String centerfile="centers.txt";
+        public float[][] centroids = new float[50][3];
+        int n=0;
+
+        public void setup(Context context) throws IOException {
+            Scanner reader = new Scanner(new FileReader(centerfile));
+            int i = 0;
+            //for (int  i=0; i<15; i++ ) {
+            while(reader.hasNext()){
+                n++;
+                int pos = reader.nextInt();
+                centroids[i][2] = pos;
+                centroids[i][0] = reader.nextFloat();
+                centroids[i][1] = reader.nextFloat();
+                i++;
+            }
+        }
+
+        public void map(LongWritable key, TwoDPointWritable value, Context context) throws IOException, InterruptedException {
+            float distance=0;
+            float mindistance=999999999.9f;
+            int winnercentroid=-1;
+            int i=0;
+
+            FloatWritable X = value.getx();
+            FloatWritable Y = value.gety();
+            float x = X.get();
+            float y = Y.get();
+            for ( i=0; i<n; i++ ) {
+                distance = (float)Math.sqrt(( x-centroids[i][0])*(x-centroids[i][0]) + (y - centroids[i][1])*(y-centroids[i][1]));
+                if ( distance < mindistance ) {
+                    mindistance = distance;
+                    winnercentroid=i;
+                }
+            }
+
+            IntWritable winnerCentroid = new IntWritable((int)centroids[winnercentroid][2]);
+            context.write(winnerCentroid, value);
+            System.out.printf("Map: Centroid = %d distance = %f\n", winnercentroid, mindistance);
+        }
+    }
+
+    public static class KmeansReducer extends Reducer<IntWritable,TwoDPointWritable,IntWritable,Text> {
+        protected void setup(Context context) throws IOException, InterruptedException {
+            BufferedWriter salida = new BufferedWriter(new FileWriter("/home/user/IdeaProjects/kmeans/data/centers.txt",false));
+            salida.close();
+        }
+
+
+        public void reduce(IntWritable clusterid, Iterable<TwoDPointWritable> points, Context context) throws IOException, InterruptedException {
+
+            int num = 0;
+            float centerx=0.0f;
+            float centery=0.0f;
+            for (TwoDPointWritable point : points) {
+                num++;
+                FloatWritable X = point.getx();
+                FloatWritable Y = point.gety();
+                float x = X.get();
+                float y = Y.get();
+                centerx += x;
+                centery += y;
+            }
+            centerx = centerx/num;
+            centery = centery/num;
+
+            /*String preres = String.format("%f %f", centerx, centery);
+            Text result = new Text(preres);
+            context.write(clusterid, result);*/
+            BufferedWriter salida = new BufferedWriter(new FileWriter("/home/user/IdeaProjects/kmeans/data/centers.txt",true));
+            salida.write(""+clusterid+" "+centerx+" "+centery+"\n");
+            salida.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //for (int i=0;i<20;i++) {
+            Configuration conf = new Configuration();
+            String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+            if (otherArgs.length != 2) {
+                System.err.println("Usage: kmeans <in> <out>");
+                System.exit(2);
+            }
+
+            Job job = new Job(conf, "kmeans");
+            Path toCache = new Path("data/centers.txt");
+
+            job.addCacheFile(toCache.toUri());
+            job.createSymlink();
+
+            job.setJarByClass(kmeans.class);
+            job.setMapperClass(KmeansMapper.class);
+            job.setReducerClass(KmeansReducer.class);
+
+            job.setInputFormatClass(TwoDPointFileInputFormat.class);
+            FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+
+            job.setMapOutputKeyClass(IntWritable.class);
+            job.setMapOutputValueClass(TwoDPointWritable.class);
+
+            FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+            job.setOutputKeyClass(IntWritable.class);
+            job.setOutputValueClass(Text.class);
+
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+            Configuration conf2 = new Configuration();
+        otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: kmeans <in> <out>");
+            System.exit(2);
+        }
+        Job job2 = new Job(conf2, "kmeans");
+
+        job2.addCacheFile(toCache.toUri());
+        job2.createSymlink();
+
+        job2.setJarByClass(kmeans.class);
+        job2.setMapperClass(KmeansMapper.class);
+        job2.setReducerClass(KmeansReducer.class);
+
+        job2.setInputFormatClass(TwoDPointFileInputFormat.class);
+        FileInputFormat.addInputPath(job2, new Path(otherArgs[0]));
+
+        job2.setMapOutputKeyClass(IntWritable.class);
+        job2.setMapOutputValueClass(TwoDPointWritable.class);
+
+        FileOutputFormat.setOutputPath(job2, new Path(otherArgs[1]+"2"));
+        job2.setOutputKeyClass(IntWritable.class);
+        job2.setOutputValueClass(Text.class);
+
+        System.exit(job2.waitForCompletion(true) ? 0 : 1);
+        //}
+    }
+}
